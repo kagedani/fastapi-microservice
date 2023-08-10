a9cb406817d7f34a44afba73314702f59f451e23
daniele.uboldi <daniele.uboldi@quantyca.it>
2023-08-10 22:07:26 +0200

    get and create movie routes

---
+++ app/data/Movie.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from app.data.Movie import Movie
+from typing import List
+
+app = FastAPI()
+
+fake_movie_db = [
+    {
+        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+        'plot': 'The surviving members of the resistance face the First Order once again.',
+        'genres': ['Action', 'Adventure', 'Fantasy'],
+        'casts': ['Daisy Ridley', 'Adam Driver']
+    }
+]
+
+
+@app.get('/', response_model=List[Movie])
+async def index():
+    return fake_movie_db
+
+
+@app.post('/', status_code=201)
+async def add_movie(payload: Movie):
+    movie = payload.__dict__
+    fake_movie_db.append(movie)
+    return {'id': len(fake_movie_db) - 1}
